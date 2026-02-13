3d6bd76db09f079fbd75373f420c739de22c0b7b
anushkaghorpade-create <anushkaghorpade_@berkeley.edu>
2026-02-13 14:57:50 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import csv 
+import time 
+import numpy as np 
+
+file = open('test.csv', 'w', newline = none) 
+
+csvwriter = csv.writer(file, delimeter=','
+
+meta = ['time','data']
+
+csvwriter.writerow(meta) 
+
+for i in range(10):
+    now = time.time()
+    value = np.random.random()
+    csvwriter.writenow([now,value])
+
+file.close() 
