fe18e96f2a57a33d424d1c217e6feb870453ac9c
anushkaghorpade-create <anushkaghorpade_@berkeley.edu>
2026-02-13 16:10:11 -0800

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -1,15 +1,14 @@
-import numpy as np
-import matplotlib
-%matplotlib inline
+import pandas as pd
 import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
 
-data = read.csv('test.csv')
-plt.figure(fig_size(10,6))
-plt.hist(data['PM Standard'], bins=15, colors='sky blue') 
+data = pd.read_csv('test.csv', comment='#')
 
-plt.title('Distribution of Concentrations') 
-plt.xlabel('PM2.5 Concentration') 
-plt.ylabel('Frequency') 
-plt.grid(axis='y', alpha ='0.75')
-plt.show 
+plt.figure(figsize=(10, 6))
+plt.hist(data['pm25_standard'], bins=15)
+
+plt.title('Distribution of Concentrations')
+plt.xlabel('PM2.5 Concentration')
+plt.ylabel('Frequency')
+plt.grid(axis='y', alpha=0.75)
+
+plt.show()
