c913a58979ddfd0c86058274d057b217836402bf
rohancreddy9 <rohancreddy@gmail.com>
2026-02-20 14:52:28 -0800

    glorified random number generator yippee
    sys.argv inputs work

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
+import time
+import numpy as np
+import sys
 
+arguments = sys.argv
+print(arguments)
+
+data_path = arguments[1]
+runtime = int(arguments[2])
+
+file = open(data_path, 'w', newline=None)
+
+csvwriter=csv.writer(file, delimiter = ',')
+
+meta = ['time,' 'data']
+csvwriter.writerow(meta)
+
+for i in range(runtime):
+    time.sleep(1)
+    now = time.time()
+    value = np.random.random()
+    csvwriter.writerow([now,value])
+
+file.close()
+
+'''
 data = pd.read_csv('test.csv', comment='#')
 
 plt.figure(figsize=(10, 6))
@@ -12,3 +38,4 @@ plt.ylabel('Frequency')
 plt.grid(axis='y', alpha=0.75)
 
 plt.show()
+'''
